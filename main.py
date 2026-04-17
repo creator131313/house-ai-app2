13c3c7fa167b6e2e731ef524e850d5881a2e2fd8
creator131313 <sjuneja.5031@gmail.com>
2026-04-18 00:32:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,16 +7,30 @@ app = FastAPI()
 
 model = joblib.load("house_model.pkl")
 
+API_KEY = "mysecret123"
+
+def verify_key(x_api_key: str):
+    if x_api_key != API_KEY:
+        raise HTTPException(status_code=401, detail="Invalid API Key")
+
 @app.get("/")
 def home():
     return FileResponse("index.html")
 
 @app.post("/predict")
-def predict(data: dict):
+def predict(data: dict, x_api_key: str = Header(None)):
+
+    # AUTH CHECK
+    verify_key(x_api_key)
+
     try:
+        # Convert input to DataFrame
         df = pd.DataFrame([data])
+
+        # Prediction
         prediction = model.predict(df)[0]
 
+        # Safety check
         if prediction < 0:
             return {"error": "Invalid input range"}
 
@@ -24,5 +38,5 @@ def predict(data: dict):
             "predicted_price": round(prediction * 100000, 2)
         }
 
-    except:
-        return {"error": "Invalid input"}
+    except Exception as e:
+        return {"error": str(e)}
